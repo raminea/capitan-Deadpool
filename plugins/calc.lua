5ce74d87a4a25b75509e84bb04b856826a1f51e5
IRO-TEAM <iroteam4240@gmail.com>
2016-05-13 12:28:52 +0430

    Create calc.lua

---
+++ plugins/calc.lua
@@ -0,0 +1,32 @@
+local function mathjs(exp)
+  local url = 'http://api.mathjs.org/v1/'
+  url = url..'?expr='..URL.escape(exp)
+  local b,c = http.request(url)
+  local text = nil
+  if c == 200 then
+    text = '= '..b
+  
+  elseif c == 400 then
+    text = b
+  else
+    text = 'error!'
+  end
+  return text
+end
+
+local function run(msg, matches)
+  return mathjs(matches[1])
+end
+
+return {
+description = "commands: / and * and ^ and + and -",
+ usage = {
+  "/calc (Formula) : Calculate a Formula",
+  "calc (Formula) : Calculate a Formula",
+  "/محاسبه (Formula) : Calculate a Formula",
+ },
+  patterns = {
+	"^[!/#](calc) (.*)$"
+  },
+  run = run
+}
