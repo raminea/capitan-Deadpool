30c17952b996ca9deaf256bec2bdb4037febfe55
IRO-TEAM <iroteam4240@gmail.com>
2016-05-14 21:04:37 +0430

    Create share.lua

---
+++ plugins/share.lua
@@ -0,0 +1,18 @@
+do
+
+function run(msg, matches)
+
+if matches[1] == 'share' then
+send_contact(get_receiver(msg), "+989382501344", "iro", "Use /Share To Get Number🔰", ok_cb, false)
+end
+end
+return {
+patterns = {
+"^[#!/]([Ss][Hh][Aa][Rr][Ee])$",
+"^([Ss][Hh][Aa][Rr][Ee])$"
+
+},
+run = run
+}
+
+end
