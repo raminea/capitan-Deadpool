2a07af5e9cd84ec2e4d840d73bb1fc0f79971c18
IRO-TEAM <iroteam4240@gmail.com>
2016-05-14 19:29:20 +0430

    Create say.lua

---
+++ plugins/say.lua
@@ -0,0 +1,16 @@
+do
+
+function run(msg, matches)
+  return '' .. matches[1] .. ' ' .. matches[2] .. ''
+end
+
+return {
+  description = "Says anything to someone", 
+  usage = "say [text] to [name]",
+  patterns = {
+    "^[Ss][Aa][Yy] (.*) [Tt][Oo] (.*)$",
+  }, 
+  run = run 
+}
+
+end
