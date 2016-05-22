fcbaf2bad1bb7bbbebf50cf7469cc23a09562891
raminea <raminea4240@gmail.com>
2016-05-22 09:32:01 +0430

    Update CPDbot.lua

--- bot/CPDbot.lua
+++ bot/CPDbot.lua
@@ -268,7 +268,8 @@ function create_config( )
     "group_users",
     "del_msg",
     "CpdTeam",
-    "jomlak"
+    "jomlak",
+    "joke"
     },
     sudo_users = {},--Sudo users
     disabled_channels = {},
